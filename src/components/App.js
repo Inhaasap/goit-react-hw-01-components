ddf378233b13d18217296f72cfd858910228b69b
Інга Юрченко <missingusya@hotmail.com>
2023-04-11 15:01:35 +0300

    Profile and Stats

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import { UserProfile } from './UserProfile';
+import user from '../user.json';
+
+import { UserStats } from './UserStats';
+import data from '../data.json';
+
+
+
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <UserProfile
+      username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
+      
+      <UserStats
+        data={data.id}
+        label={data.label}
+        percentage={data.percentage}
+      />
+
+    </div> 
+  );  
+};
--- src/index.js
+++ src/index.js
@@ -1,58 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-import ReactDOM from "react-dom";
+// import PropTypes from 'prop-types';
+// import ReactDOM from "react-dom";
 // import paintings from "./user.json";
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-     
-// const data = painting[0];
-const data = {
-  "username": "Jacques Gluke",
-  "tag": "jgluke",
-  "location": "Ocho Rios, Jamaica",
-  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  "stats": {
-    "followers": 5603,
-    "views": 4827,
-    "likes": 1308
-  }
-};
-
-const painting = (
-  <div class="profile">
-    <div class="description">
-      <img
-        src={data.avatar}
-        alt={data.username}
-        class="avatar"
-      />
-      <p class="name">{data.username}</p>
-      <p class="tag">{data.tag}</p>
-      <p class="location">{data.location}</p>
-    </div>
-
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{data.stats.followers}</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">{data.stats.views}</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{data.stats.likes}</span>
-      </li>
-    </ul>
-  </div>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-ReactDOM.render(painting, document.querySelector('#root'));
+// ReactDOM.render(painting, document.querySelector('#root'));
